30ce389041868faf0ffd27671bb972f3236b2e1f
Meredith Trevino <meredithtrevino@gmail.com>
2023-02-01 15:47:44 -0700

    Adds completed feature user story one

--- app/controllers/coffee_shops_controller.rb
+++ app/controllers/coffee_shops_controller.rb
@@ -1,5 +1,5 @@
 class CoffeeShopsController < ApplicationController
   def index
-    
+    @coffee_shops = CoffeeShop.all
   end
 end
--- spec/features/coffee_shops/index_spec.rb
+++ spec/features/coffee_shops/index_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "coffee shop index page", type: :feature do 
-#   User Story 1, Parent Index 
+  describe "as a visitor" do
+    describe "When I visit '/parents'" do
+      it "Then I see the name of each parent record in the system" do
+        coffee_shop_1 = CoffeeShop.create!(name: "Starbucks", open_year: 1971, open_after_five: true )
 
-# For each parent table
-# As a visitor
-# When I visit '/parents'
-# Then I see the name of each parent record in the system
+        visit '/coffee_shops'
+        expect(page).to have_content(coffee_shop_1.name)
+      end
+    end
+  end
 end
