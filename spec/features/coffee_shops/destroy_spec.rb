3dc7df7048aa8d7e8527a3d0d5a5a080f8a245df
Meredith Trevino <meredithtrevino@gmail.com>
2023-02-07 12:17:22 -0700

    user story 19

--- spec/features/coffee_shops/destroy_spec.rb
+++ spec/features/coffee_shops/destroy_spec.rb
@@ -12,13 +12,13 @@ require 'rails_helper'
 
 RSpec.describe "Parent delete" do
   describe 'when I visit parent show page, I see a link to delete parent, click the link' do
-    xit "Then a 'DELETE' request is sent to '/parents/:id'" do
+    it "Then a 'DELETE' request is sent to '/parents/:id'" do
       coffee_shop = CoffeeShop.create!(name: "Hooked on Colfax")
 
       visit "/coffee_shops/#{coffee_shop.id}"
       click_button "Delete"
       
-      expect(current_path).to eq("/coffee_shops")
+      expect(current_path).to eq("/coffee_shops/")
       expect(page).to_not have_content("Hooked on Colfax")
     end
   end
